82809cd8a9ff13537a62b30c6fe20c3bec42fb0c
eliseusbrito <eliseusbrito@gmail.com>
2020-12-10 08:02:19 -0300

    Added Package's DTO request and Mapper with mapstruct.org

---
+++ src/main/java/com/eliseubrito/personapi/mapper/PersonMapper.java
@@ -0,0 +1,19 @@
+package com.eliseubrito.personapi.mapper;
+
+import com.eliseubrito.personapi.dto.request.PersonDTO;
+import com.eliseubrito.personapi.model.Person;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface PersonMapper {
+
+    PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
+
+    @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
+    Person toModel(PersonDTO personDTO);
+
+    PersonDTO toDTO(Person person);
+
+}
--- src/main/java/com/eliseubrito/personapi/service/PersonService.java
+++ src/main/java/com/eliseubrito/personapi/service/PersonService.java
@@ -1,6 +1,8 @@
 package com.eliseubrito.personapi.service;
 
 import com.eliseubrito.personapi.dto.MessageResponseDTO;
+import com.eliseubrito.personapi.dto.request.PersonDTO;
+import com.eliseubrito.personapi.mapper.PersonMapper;
 import com.eliseubrito.personapi.model.Person;
 import com.eliseubrito.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,13 +13,16 @@ public class PersonService {
 
     private PersonRepository personRepository;
 
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
+
     @Autowired
     public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
 
-    public MessageResponseDTO createPerson(Person person) {
-        Person savedPerson = personRepository.save(person);
+    public MessageResponseDTO createPerson(PersonDTO personDTO) {
+        Person personToSave = personMapper.toModel(personDTO);
+        Person savedPerson = personRepository.save(personToSave);
         return MessageResponseDTO
                 .builder()
                 .message("Created person with ID " + savedPerson.getId())
