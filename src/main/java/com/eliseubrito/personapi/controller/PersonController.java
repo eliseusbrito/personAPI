cd793b8f2a7b41c06b27a331e21f16c95572ce6d
eliseusbrito <eliseusbrito@gmail.com>
2020-12-09 21:02:33 -0300

    Add PersonService Class and refactored PersonController

--- src/main/java/com/eliseubrito/personapi/controller/PersonController.java
+++ src/main/java/com/eliseubrito/personapi/controller/PersonController.java
@@ -2,31 +2,26 @@ package com.eliseubrito.personapi.controller;
 
 import com.eliseubrito.personapi.dto.MessageResponseDTO;
 import com.eliseubrito.personapi.model.Person;
-import com.eliseubrito.personapi.repository.PersonRepository;
+import com.eliseubrito.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-    private PersonRepository personRepository;
+    private PersonService personService;
 
     @Autowired
-    public PersonController(PersonRepository personRepository) {
-        this.personRepository = personRepository;
+    public PersonController(PersonService personService) {
+        this.personService = personService;
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createPerson(@RequestBody Person person) {
-        Person savedPerson = personRepository.save(person);
-        return MessageResponseDTO
-                .builder()
-                .message("Created person with ID " + savedPerson.getId())
-                .build();
+        return personService.createPerson(person);
     }
 
 }
---
+++ src/main/java/com/eliseubrito/personapi/service/PersonService.java
@@ -0,0 +1,27 @@
+package com.eliseubrito.personapi.service;
+
+import com.eliseubrito.personapi.dto.MessageResponseDTO;
+import com.eliseubrito.personapi.model.Person;
+import com.eliseubrito.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public MessageResponseDTO createPerson(Person person) {
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + savedPerson.getId())
+                .build();
+    }
+
+}
