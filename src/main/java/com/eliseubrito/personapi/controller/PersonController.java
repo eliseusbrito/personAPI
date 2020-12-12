fb2442a5af409b7d501c622d8c631d4a9050f4ca
eliseusbrito <eliseusbrito@gmail.com>
2020-12-12 14:45:54 -0300

    Lombok anotations for eliminate constructor

--- src/main/java/com/eliseubrito/personapi/controller/PersonController.java
+++ src/main/java/com/eliseubrito/personapi/controller/PersonController.java
@@ -4,6 +4,7 @@ import com.eliseubrito.personapi.dto.MessageResponseDTO;
 import com.eliseubrito.personapi.dto.request.PersonDTO;
 import com.eliseubrito.personapi.exception.PersonNotFoundException;
 import com.eliseubrito.personapi.service.PersonService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,14 +14,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonController {
 
     private PersonService personService;
-
-    @Autowired
-    public PersonController(PersonService personService) {
-        this.personService = personService;
-    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
